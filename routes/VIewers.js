b0f843542dfcdb6e76e3cb8830fe3ef68e19156e
Kenneth Dimalibot <dimalibotkennethofficial@gmail.com>
2023-12-12 23:32:47 +0800

    fsdfsd

--- routes/VIewers.js
+++ routes/VIewers.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
+//#Router get for response(res)
+// router.get('/Viewers', (req, res) => {
+//     res.json({
+//         "name" : "Kenneth",
+//         "gender" : "Male",
+//         "age" : "23"
+//     })
+// });
+
+//#Router get for request(req)
 router.get('/Viewers', (req, res) => {
-    res.send("List of Viewers")
+    console.log(req.query)
 });
+
+router.post('/Insert', (req, res) => {
+    res.json({
+        "message" : "Success!",
+        
+    })
+});
+
+
+
 
 module.exports = router
 
