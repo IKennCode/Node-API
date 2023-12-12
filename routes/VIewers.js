810b408b222f302459f87a55b97dfd0b91a21536
Kenneth Dimalibot <dimalibotkennethofficial@gmail.com>
2023-12-12 21:16:49 +0800

    new update

---
+++ routes/VIewers.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/getViewers', (req,res) => {
+    response.send("List of Viewers")
+});
+
+module.exports = router;
